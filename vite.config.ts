e6931eb30cdfa9ed37b3b02ef5cd16922a9484f7
CyberNinja10291 <bluedragon10291@gmail.com>
2024-04-17 20:41:49 +0300

    updated new files

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import EnvironmentPlugin from "vite-plugin-environment";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    EnvironmentPlugin({
-      // Uses 'development' if the NODE_ENV environment variable is not defined.
-      NODE_ENV: "development",
-
-      // Have in mind that variables coming from process.env are always strings.
-      DEBUG: "false",
-    }),
-  ],
-  resolve: {
-    alias: {
-      // Provide aliases for node modules
-      stream: "stream-browserify",
-      crypto: "crypto-browserify",
-      zlib: "browserify-zlib",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env": env,
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        // Provide aliases for node modules
+        stream: "stream-browserify",
+        crypto: "crypto-browserify",
+        zlib: "browserify-zlib",
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
     },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: "globalThis",
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+          }),
+        ],
+      },
     },
-  },
+  };
 });
